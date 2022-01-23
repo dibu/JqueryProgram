1511d3a35bbe9b3516565d9efc629478bba81598
Dibyajyoti Das <dibu.aspnet@hotmail.com>
2022-01-23 21:30:34 +0530

    Grid edit and save

--- JqueryProgram.Api/Controllers/EmployeeController.cs
+++ JqueryProgram.Api/Controllers/EmployeeController.cs
@@ -22,14 +22,7 @@ namespace JqueryProgram.Api.Controllers
                 return hostingEnvironment.ContentRootPath + "/JsonDB/Employee.json";
             }
         }
-        private string DBContent {
-            get {
-                return JsonConvert.SerializeObject(GetAllEmployee());
-            }
-            set {
-                _dbContent = value;
-            }
-        }
+       
         public EmployeeController(IWebHostEnvironment hosting)
         {
             this.hostingEnvironment = hosting;
@@ -44,10 +37,18 @@ namespace JqueryProgram.Api.Controllers
         {
             return GetEmployees();
         }
-        [HttpPost]
-        public bool SaveEmployee([FromBody] Employee employee) {
+        [HttpPost("SaveEmployee")]
+        public bool SaveEmployee(Employee employee) {
             bool isSaved = false;
-
+            List<Employee> empList = GetEmployees();
+            var selectedEmp = empList.Where(e => e.Id == employee.Id).FirstOrDefault();
+            selectedEmp.Name = employee.Name;
+            selectedEmp.Email = employee.Email;
+            selectedEmp.Designation = employee.Designation;
+            selectedEmp.Phone = employee.Phone;
+            empList[employee.Id - 1] = employee;
+            string content = JsonConvert.SerializeObject(empList);
+            this.SaveChanges(content, ref isSaved);
             return isSaved;
         }
         [HttpGet("GetEmpById/{empId}")]
@@ -55,11 +56,24 @@ namespace JqueryProgram.Api.Controllers
           return  GetEmployees().Where(e => e.Id == empId).FirstOrDefault();
         }
 
+        [HttpGet("GetDesignations")]
+        public List<string> GetDesignations()
+        {
+            List<string> designations = new List<string>();
+            designations.Add("Developer");
+            designations.Add("Manager");
+            designations.Add("Architect");
+            designations.Add("Tester");
+            designations.Add("Designer");
+            designations.Add("Deliver manager");
+            return designations;
+        }
         private List<Employee> GetEmployees() {
             List<Employee> emp = null;
             using (StreamReader reader = new StreamReader(this.GetDBPath))
             {
                 emp = JsonConvert.DeserializeObject<List<Employee>>(reader.ReadToEnd());
+                
             }
             return emp;
         } 
@@ -94,7 +108,21 @@ namespace JqueryProgram.Api.Controllers
 
         }
 
-        private void SaveChanges() { 
+
+        private void SaveChanges(string content, ref bool isSaved) {
+            string filePath = hostingEnvironment.ContentRootPath + "/JsonDB/Employee.json";
+            try
+            {
+                isSaved = true;
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    writer.Write(content);
+                }
+            }
+            catch (Exception exp) {
+                isSaved = false;
+                throw exp;
+            }
             
         }
         
--- JqueryProgram/scipts/EmployeeGridView.js
+++ JqueryProgram/scipts/EmployeeGridView.js
@@ -4,6 +4,7 @@
             console.log(data);
             GridView.createGridView(data);
         });
+        GridView.getDesignations();
     },
     createGridView(data) {
         var html = [];
@@ -21,11 +22,80 @@
                 $.each(d, function (k, v) {
                     html.push('<td>' + v + '</td>');
                 }); 
-                html.push('<td><button type="button">Edit</button></td>');
+                html.push('<td><button type="button" onclick="GridView.eidtClick(this,\''+ d.id + '\');">Edit</button></td>');
                 html.push('</tr>');
             })
             html.push('</table>');
         }
         $("#gridview").html(html.join(''));
+    },
+    eidtClick: function (ref, id) {
+        var btnText = $(ref).html();
+        if (btnText == 'Edit') {
+            var tr = $(ref).parent().parent();
+            var name = $(tr).find('td:eq(2)').html();
+            var email = $(tr).find('td:eq(3)').html();
+            var designation = $(tr).find('td:eq(4)').html();
+            var phone = $(tr).find('td:eq(5)').html();
+            var designationsHTML = [];
+            $(window.designations).each(function (i, v) {
+                designationsHTML.push('<option value=' + v + '>' + v + '</option>');
+            });
+            var tds = $(ref).parent().siblings();
+            $(tds).eq('2').html('<input type="text" value=' + name + '>');
+            $(tds).eq('3').html('<input type="text" value=' + email + '>');
+            $(tds).eq('5').html('<input type="text" value=' + phone + '>');
+            $(tds).eq('4').html('<select>' + designationsHTML.join('') + '</select>');
+            $(ref).html('Save');
+        } else {
+            var tds = $(ref).parent().siblings();
+            var name = $(tds).eq('2').find('input[type="text"]').val();
+            var email = $(tds).eq('3').find('input[type="text"]').val();
+            var phone = $(tds).eq('5').find('input[type="text"]').val();
+            var designation = $(tds).eq('4').find('select').find('option:selected').val();
+
+            var employee = {
+                'Id' : parseInt(id),
+                'Name': name,
+                'Email': email,
+                'Designation': designation,
+                'Phone': phone
+            };
+            console.log(JSON.stringify(employee));
+            GridView.saveEmployee(employee, ref);
+        }
+        
+    },
+    getDesignations: function () {
+        $.get("http://localhost:21117/api/employee/GetDesignations", function (data) {
+            console.log(data);
+            window.designations = data;
+        });
+    },
+    saveEmployee: function (obj, ref) {
+      //  $.post("http://localhost:21117/api/employee/SaveEmployee",JSON.stringify(obj));
+        $.ajax({
+            type: "POST",
+            processData: false,
+            contentType: 'application/json',
+            crossDomain: true,
+            dataType: 'json',
+            data: JSON.stringify(obj),
+            url: "http://localhost:21117/api/employee/SaveEmployee",
+            success: function (response) {
+                if (response) {
+                    $(ref).html('Edit');
+
+                    var tds = $(ref).parent().siblings();
+                    $(tds).eq('2').html(obj.Name);
+                    $(tds).eq('3').html(obj.Email);
+                    $(tds).eq('5').html(obj.Phone);
+                    $(tds).eq('4').html(obj.Designation);
+                }
+            },
+            error: function (err) {
+                alert('Error', err);
+            }
+        });
     }
 }
