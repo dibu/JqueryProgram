a362eb7951108933f1c6eee7e38914351481c63d
Dibyajyoti Das <dibu.aspnet@hotmail.com>
2022-03-06 20:50:06 +0530

    deferred test

---
+++ JqueryProgram/DefferedTest.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DefferedTest.aspx.cs" Inherits="JqueryProgram.DefferedTest" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="maindiv">
+        </div>
+    </form>
+  <script src="scipts/jquery-3.6.0.js"></script>
+  <script src="scipts/DefferedTest.js"></script>
+</body>
+</html>
---
+++ JqueryProgram/scipts/DefferedTest.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    DeferredTest.createDropDown(DeferredTest.populateDropDown);
+});
+
+var DeferredTest = {
+    createDropDown: function (callback) {
+        $('#maindiv').append('<select id="drop-designation"></select>');
+        if (callback) {
+            callback();
+        }
+    },
+    populateDropDown: function () {
+        var deffered = $.Deferred();
+        $.get("http://localhost:21117/api/employee/GetDesignations", function (data) {
+            if (data.length > 0) {
+                var designationsHTML = [];
+                $(data).each(function (i, v) {
+                    designationsHTML.push('<option value=' + v + '>' + v + '</option>');
+                });
+                $('#drop-designation').html(designationsHTML.join());
+                setTimeout(function () {
+                    deffered.resolve(DeferredTest.setDesignation('Tester'));
+                }, 10000)
+               
+            }           
+        });
+     
+    },
+
+    setDesignation: function (designation) {
+        $('#drop-designation').val(designation);
+    }
+}
